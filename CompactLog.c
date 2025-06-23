8d7a95de4cd249a2e7b9a4f81e5d458d6d75251a
RinPark1512 <112235129+RinPark1512@users.noreply.github.com>
2025-06-23 15:23:17 -0700

    clean:

--- CompactLog.c
+++ CompactLog.c
@@ -1,7 +1,10 @@
+/* add in file comments, copyright (google)*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/errno.h>
+// define 5, using macro
 
 void compactLine(char*, char*);
 
@@ -12,6 +15,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     if (argc != 5){
+        // also add requirements checking -i -o
         printf("The command syntax is:\n\nCompactLog -i  {input_logfile} -o {output_logfile} \nCompactLog -h  // help syntax\nCompactLog // help syntax\n");
         return 0;
     }
@@ -25,30 +29,33 @@ int main(int argc, char *argv[]) {
     // diff, print line + count into out file
     printf("Task Complete! Come Again!\n");
 
-
     return 0;
 }
-
+// some return value so parent knows if it succeed, fail
 void compactLine(char* inputF, char* outputF){
-    FILE* inputFile = fopen(inputF,"r");;
+    FILE* inputFile = fopen(inputF,"r");
     FILE* outputFile = fopen(outputF, "w");
     if(NULL == inputFile || NULL == outputFile){
         fprintf(stderr, "Error opening file: %s: %s \n", inputF, strerror(errno));
         return;
     }
+    // define 200 as well
+    // no magic number
     char line[200];
     char* prev = NULL;
     int count = 1;
     while (fgets(line, 200, inputFile)) {
         // printf("line: %s\n", line);
         line[strcspn(line, "\r\n")] = '\0';
-        if (prev == NULL){
+        if (prev == NULL) {
             prev = strdup(line);
-        }else if(strcmp(line, prev) == 0){
+        } else if(strcmp(line, prev) == 0) {
             count++;
-        } else{
+        } else {
             // printf("count: %d\n", count);
-            fprintf(outputFile,"%s * %d\n", prev, count);
+            if (count > 1) {
+                fprintf(outputFile,"%s * %d\n", prev, count);
+            }
             fflush(outputFile);
             prev = strdup(line);
             count = 1;
